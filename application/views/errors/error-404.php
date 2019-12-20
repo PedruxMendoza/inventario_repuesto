44ee362d9e239342cba7d246c09a66cf96fd86ae
isidro <isidrosalvador1@gmail.com>
2019-12-20 09:13:54 -0600

    se agrego pagina de error-404

---
+++ application/views/errors/error-404.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url('props/assets/images/favicon.png') ?>">
+    <title>Inventario || 404</title>
+    <!-- Custom CSS -->
+    <link href="<?php echo base_url('props/dist/css/style.min.css') ?>" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+
+<body>
+    <div class="main-wrapper">
+        <!-- ============================================================== -->
+        <!-- Preloader - style you can find in spinners.css -->
+        <!-- ============================================================== -->
+        <div class="preloader">
+            <div class="lds-ripple">
+                <div class="lds-pos"></div>
+                <div class="lds-pos"></div>
+            </div>
+        </div>
+        <!-- ============================================================== -->
+        <!-- Preloader - style you can find in spinners.css -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Login box.scss -->
+        <!-- ============================================================== -->
+        <div class="error-box" style="background-color: #F4D74C;">
+            <div class="error-body text-center">
+                <h1 class="text error-subtitle font-weight-bold" style="color: white; font-size: 75px">Oops!</h1>
+                <img src="<?php echo base_url('props/images/logo404.png') ?>" width="500px">
+                <h1 class="text-uppercase m-t-30 m-b-30" style="color: black; font-size: 25px"><strong class="font-weight-bold" style="font-size: 30px">Error:</strong> La pagina que deseas ver no esta disponible</h1>
+                <a href="<?php echo base_url('Welcome/index');?>" class="btn btn-danger btn-rounded waves-effect waves-light m-b-40">Regresar al Inicio</a> </div>
+            </div>
+            <!-- ============================================================== -->
+            <!-- Login box.scss -->
+            <!-- ============================================================== -->
+            <!-- ============================================================== -->
+            <!-- Page wrapper scss in scafholding.scss -->
+            <!-- ============================================================== -->
+            <!-- ============================================================== -->
+            <!-- Page wrapper scss in scafholding.scss -->
+            <!-- ============================================================== -->
+            <!-- ============================================================== -->
+            <!-- Right Sidebar -->
+            <!-- ============================================================== -->
+            <!-- ============================================================== -->
+            <!-- Right Sidebar -->
+            <!-- ============================================================== -->
+        </div>
+        <!-- ============================================================== -->
+        <!-- All Required js -->
+        <!-- ============================================================== -->
+        <script src="<?php echo base_url('props/assets/libs/jquery/dist/jquery.min.js') ?>"></script>
+        <!-- Bootstrap tether Core JavaScript -->
+        <script src="<?php echo base_url('props/assets/libs/popper.js/dist/umd/popper.min.js') ?>"></script>
+        <script src="<?php echo base_url('props/assets/libs/bootstrap/dist/js/bootstrap.min.js') ?>"></script>
+        <!-- ============================================================== -->
+        <!-- This page plugin js -->
+        <!-- ============================================================== -->
+        <script>
+            $('[data-toggle="tooltip"]').tooltip();
+            $(".preloader").fadeOut();
+        </script>
+    </body>
+
+    </html>
